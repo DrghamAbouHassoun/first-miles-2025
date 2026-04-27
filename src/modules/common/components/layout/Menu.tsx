1225421e7d7a00a4035e4391235ef8607b5d940e
DerghamDev <drgham.m@mediaslide.com>
2026-04-27 14:18:59 +0400

    Fix contact menu

--- src/modules/common/components/layout/Menu.tsx
+++ src/modules/common/components/layout/Menu.tsx
@@ -5,15 +5,22 @@ import { useTranslation } from "../../hooks/useTranslation";
 import { MenuContext } from "../../contexts/MenuProvider";
 import { X } from "lucide-react";
 import { socialLinksData } from "./SocialLinks";
+import { ContactModalContext } from "../../../home/contexts/ContactModalProvider";
 
 const Menu = () => {
   const { navigate, currentRoute } = useContext(RouterContext);
   const { t } = useTranslation("common");
   const { isOpen, toggleMenu } = useContext(MenuContext);
+  const { toggleModel } = useContext(ContactModalContext);
 
   useEffect(() => {
     toggleMenu(false);
   }, [currentRoute])
+
+  const openInfoModal = () => {
+    toggleMenu(false)
+    toggleModel(true);
+  }
 
   return (
     <div className={`w-full h-screen overflow-y-auto fixed z-50 top-0 left-0 bg-fm-green flex flex-col justify-center items-center gap-8 ${isOpen ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"} transition-all duration-700`}>
@@ -32,7 +39,16 @@ const Menu = () => {
         ))}
       </div>
       <div className={`flex bg-fm-yellow rounded-md `}>
-        {socialLinksData.map((link) => (
+        {socialLinksData.map((link) => link.type === "button" ? (
+          <button
+            key={link.name}
+            type="button"
+            className="flex items-center p-2 w-9.5 h-9.5"
+            onClick={() => openInfoModal()}
+          >
+            {link.icon}
+          </button>
+        ) :(
           <a
             href={link.href}
             target="_blank"
--- src/modules/home/components/ContactModal.tsx
+++ src/modules/home/components/ContactModal.tsx
@@ -1,5 +1,11 @@
 import { useTranslation } from "../../common/hooks/useTranslation";
-import { ADDRESS, EMAIL, PHONE_NUMBER, PHONE_NUMBER_DISPLAY, PO_BOX } from "../../../config/constants";
+import {
+  ADDRESS,
+  EMAIL,
+  PHONE_NUMBER,
+  PHONE_NUMBER_DISPLAY,
+  PO_BOX,
+} from "../../../config/constants";
 import { X } from "lucide-react";
 import { useContext } from "react";
 import { ContactModalContext } from "../contexts/ContactModalProvider";
@@ -14,17 +20,47 @@ const ContactModal = () => {
       className={`w-full h-screen fixed top-0 left-0 bg-black/25 z-40 flex justify-start items-center ${isOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} transition-opacity duration-500`}
     >
       <div className="w-full h-fit max-w-90 py-12 p-4 bg-fm-yellow relative">
-        <button onClick={() => toggleModel(false)} className={`absolute top-4 ${lang === "ar" ? "left-4" : "right-4"} p-1`}>
+        <button
+          onClick={() => toggleModel(false)}
+          className={`absolute top-4 ${lang === "ar" ? "left-4" : "right-4"} p-1`}
+        >
           <X size={24} />
         </button>
-        <p className="mb-2"><b>{t("contact.investors")}</b></p>
-        <p className="mb-2"><b>{t("contact.address")}:</b> {ADDRESS}</p>
+        <p className="mb-2">
+          <b>{t("contact.investors")}</b>
+        </p>
+        <p className="mb-2">
+          <b>{t("contact.address")}:</b> {ADDRESS}
+        </p>
 
-        <p className="mb-2"><b>{t("contact.phone")}:</b> <a href={`tel:${PHONE_NUMBER}`} target="_blank" rel="noopener noreferrer" className="text-fm-gray-400">{PHONE_NUMBER_DISPLAY}</a></p>
+        <p className="mb-2">
+          <b>{t("contact.phone")}:</b>{" "}
+          <a
+            href={`tel:${PHONE_NUMBER}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-fm-gray-400"
+            dir={lang === "ar" ? "ltr" : "ltr"}
+          >
+            {PHONE_NUMBER_DISPLAY}
+          </a>
+        </p>
 
-        <p className="mb-2"><b>{t("contact.po_box")}:</b> {PO_BOX}</p>
+        <p className="mb-2">
+          <b>{t("contact.po_box")}:</b> {PO_BOX}
+        </p>
 
-        <p><b>{t("contact.email")}:</b> <a href={`mailto:${EMAIL}`} target="_blank" rel="noopener noreferrer" className="text-fm-gray-400">{EMAIL}</a></p>
+        <p>
+          <b>{t("contact.email")}:</b>{" "}
+          <a
+            href={`mailto:${EMAIL}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-fm-gray-400"
+          >
+            {EMAIL}
+          </a>
+        </p>
       </div>
     </div>
   );
